9a34f369bc16637857beaff68489d2fae6217acd
Giso Grimm <g.grimm@uni-oldenburg.de>
2024-07-27 16:58:17 +0200

    update dependencies of TASCAR tap

--- Formula/tascar.rb
+++ Formula/tascar.rb
@@ -6,26 +6,30 @@ class Tascar < Formula
 
   license "GPL-2.0"
 
+  depends_on "adwaita-icon-theme"
+  depends_on "boost" => :build
+  depends_on "cmake" => :build
   depends_on "coreutils" => :build # needed for the "realpath" command
+  depends_on "cunit"
   depends_on "eigen"
   depends_on "fftw"
+  depends_on "glibmm"
   depends_on "gsl"
   depends_on "gtkmm3"
   depends_on "gtksourceviewmm3"
   depends_on "jack"
-  #depends_on "labstreaminglayer/tap/lsl"
+  depends_on "jpeg"
+  depends_on "labstreaminglayer/tap/lsl"
   depends_on "liblo"
   depends_on "libltc"
   depends_on "libmatio"
+  depends_on "libsamplerate"
   depends_on "libsndfile"
+  depends_on "make" => :build
   depends_on "pkgconfig" => :build
-  depends_on "portaudio"
   depends_on "qjackctl" => :recommended
+  depends_on "vim" => :build
   depends_on "xerces-c"
-
-  # cmake, boost needed for the unit tests. We execute those during homebrew build.
-  depends_on "cmake" => :build
-  depends_on "boost" => :build
 
   def install
     system "HOMEBREW_TASCAR_TAG=#{TASCAR_TAG} make PREFIX=#{prefix} homebrew"
