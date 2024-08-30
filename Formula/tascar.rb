e341f8e3c51a99c96a3bbfb0b8d0281792dc96d4
Giso Grimm <giso.grimm@vegri.net>
2024-08-30 08:31:50 +0200

    release 0.233.0

--- Formula/tascar.rb
+++ Formula/tascar.rb
@@ -1,5 +1,5 @@
 class Tascar < Formula
-  TASCAR_TAG = "brewdev_0.232.2_c"
+  TASCAR_TAG = "release_0.233.0"
   desc "Toolbox for Acoustic Scene Creation and Rendering (tascar)"
   homepage "https://tascar.org/"
   url "https://github.com/gisogrimm/tascar", :using => :git, :tag => TASCAR_TAG
