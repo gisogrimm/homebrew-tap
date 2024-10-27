1363c7b94ea475eea8119f7817cd852f2cf841ff
Giso Grimm <giso.grimm@vegri.net>
2024-10-27 16:07:41 +0100

    initial formula for ovbox

---
+++ Formula/ovbox.rb
@@ -0,0 +1,37 @@
+class Ovbox < Formula
+  OVBOX_TAG = "v0.25.1_brewdev1"
+  desc "ORLANDOviols consort box remote music collaboration software (OVBOX)"
+  homepage "https://ovbox.de/"
+  url "https://github.com/gisogrimm/ov-client", :using => :git, :tag => OVBOX_TAG
+
+  license "GPL-2.0"
+
+  depends_on "adwaita-icon-theme"
+  depends_on "boost" => :build
+  depends_on "cmake" => :build
+  depends_on "coreutils" => :build # needed for the "realpath" command
+  depends_on "cunit"
+  depends_on "eigen"
+  depends_on "fftw"
+  depends_on "glibmm"
+  depends_on "gsl"
+  depends_on "gtkmm3"
+  depends_on "gtksourceviewmm3"
+  depends_on "jack"
+  depends_on "jpeg"
+  depends_on "labstreaminglayer/tap/lsl"
+  depends_on "liblo"
+  depends_on "libltc"
+  depends_on "libmatio"
+  depends_on "libsamplerate"
+  depends_on "libsndfile"
+  depends_on "make" => :build
+  depends_on "pkgconfig" => :build
+  depends_on "qjackctl" => :recommended
+  depends_on "vim" => :build
+  depends_on "xerces-c"
+
+  def install
+    system "HOMEBREW_OVBOX_TAG=#{OVBOX_TAG} make -j `nproc` PREFIX=#{prefix} homebrew"
+  end
+end
