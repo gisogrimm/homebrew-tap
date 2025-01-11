5b21eb632bb2aec8eb25df4946bc091f60142add
Giso Grimm <giso.grimm@vegri.net>
2025-01-11 18:09:28 +0100

    release 0.234.0

--- Formula/tascar.rb
+++ Formula/tascar.rb
@@ -1,5 +1,5 @@
 class Tascar < Formula
-  TASCAR_TAG = "release_0.233.2.1"
+  TASCAR_TAG = "release_0.234.0"
   desc "Toolbox for Acoustic Scene Creation and Rendering (tascar)"
   homepage "https://tascar.org/"
   url "https://github.com/gisogrimm/tascar", :using => :git, :tag => TASCAR_TAG
