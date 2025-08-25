5ed2a148d6fbeb8dced0c7754544e0a432ed0808
Giso Grimm <giso.grimm@vegri.net>
2025-08-25 22:16:46 +0200

    use latest TASCAR version to avoid failure due to recent cmake

--- Formula/tascar.rb
+++ Formula/tascar.rb
@@ -1,5 +1,5 @@
 class Tascar < Formula
-  TASCAR_TAG = "release_0.234.2"
+  TASCAR_TAG = "release_0.234.3"
   desc "Toolbox for Acoustic Scene Creation and Rendering (tascar)"
   homepage "https://tascar.org/"
   url "https://github.com/gisogrimm/tascar", :using => :git, :tag => TASCAR_TAG
