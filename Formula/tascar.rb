43cb568ef357e82783e492f2b8a81e6d2224751f
Giso Grimm (thetys) <g.grimm@uni-oldenburg.de>
2025-10-28 10:59:11 +0100

    release of TASCAR 0.235.1

--- Formula/tascar.rb
+++ Formula/tascar.rb
@@ -1,5 +1,5 @@
 class Tascar < Formula
-  TASCAR_TAG = "release_0.234.4"
+  TASCAR_TAG = "release_0.235.1"
   desc "Toolbox for Acoustic Scene Creation and Rendering (tascar)"
   homepage "https://tascar.org/"
   url "https://github.com/gisogrimm/tascar", :using => :git, :tag => TASCAR_TAG
