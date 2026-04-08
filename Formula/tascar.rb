6c02225f7e5234a5e9057261cb8459b8274bf852
Giso Grimm (thetys) <g.grimm@uni-oldenburg.de>
2026-04-08 10:35:29 +0200

    TASCAR release 0.237.1

--- Formula/tascar.rb
+++ Formula/tascar.rb
@@ -1,5 +1,5 @@
 class Tascar < Formula
-  TASCAR_TAG = "feature_homebrew_packaging_0.236.0"
+  TASCAR_TAG = "feature_homebrew_packaging_0.237.1"
   desc "Toolbox for Acoustic Scene Creation and Rendering (tascar)"
   homepage "https://tascar.org/"
   url "https://github.com/gisogrimm/tascar", :using => :git, :tag => TASCAR_TAG
